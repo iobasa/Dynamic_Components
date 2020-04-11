610086b4abfc241b1b4a466428da0f4c86f4bac7
iobasa <i_obasa@fanshaweonline.ca>
2020-04-11 16:09:53 -0400

    toggle switch

--- js/main.js
+++ js/main.js
@@ -4,9 +4,13 @@ import VideoComponent from "./components/TheVideoComponent.js";
 (() => {
     const myVM = new Vue({
         data: {
+            activeComponent: VideoComponent
         },
 
         methods: {
+            switchComponents(){
+                this.activeComponent = (this.activeComponent.name === "VideoComponent") ? AudioComponent : VideoComponent;
+            }
         }
     }).$mount("#app");
 })();
